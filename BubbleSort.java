c78594b06f97685bd47992b528a66ebb71897c1c
Vidhan Chandra <vidhansharma01@users.noreply.github.com>
2018-08-31 01:52:48 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.util.*;
+
+import static java.lang.System.out;
+public class Spam {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    static class Print {
+        private BufferedWriter bw;
+
+        public Print() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(out));
+        }
+
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+
+        public void close() throws IOException {
+            bw.close();
+        }
+
+    }
+
+    public static void main(String args[]) throws IOException {
+        FastReader fr = new FastReader();
+        Print print = new Print();
+        int N = fr.nextInt();
+        int a[] = new int[N];
+        for (int i = 0; i < N; i++){
+            a[i] = fr.nextInt();
+        }
+        for (int i = 0; i < N-1; i++){
+            for (int j = 0; j < N-i-1; j++){
+                if (a[j] > a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+        for (int i = 0;i < N ;i++ ) {
+        	print.println(a[i]);
+        }
+        print.close();
+    }
+
